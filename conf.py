8c7e96ddf00a3ffab8c47df9fa3ce9c40a436c88
Henry Hirsch <henry@w3-net.de>
2014-12-19 07:51:53 +0100

    [fix] New config.

--- conf.py
+++ conf.py
@@ -325,11 +325,11 @@ REDIRECTIONS = []
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
-# GITHUB_SOURCE_BRANCH = 'master'
-# GITHUB_DEPLOY_BRANCH = 'gh-pages'
+GITHUB_SOURCE_BRANCH = 'source'
+GITHUB_DEPLOY_BRANCH = 'master'
 
 # The name of the remote where you wish to push to, using github_deploy.
-# GITHUB_REMOTE_NAME = 'origin'
+GITHUB_REMOTE_NAME = 'origin'
 
 # If you really care about history in the GitHub Pages branch, you can
 # set this to True to `git pull` before changes are made.
